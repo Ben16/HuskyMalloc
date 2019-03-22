f150c817f3fe8a764cfde59fe51881e21292d142
Michael Burrowes <burrowes.mic@husky.neu.edu>
2019-03-22 00:30:24 -0400

    fixed bitfield sizing issue; multi-thread next

--- par_malloc.c
+++ par_malloc.c
@@ -11,7 +11,8 @@
 #define BUCKET_COUNT 10
 
 const int bucket_sizes[BUCKET_COUNT] = {4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
-const int bitfield_sizes[BUCKET_COUNT] = {124, 63, 32, 16, 8, 4, 2, 1, 1, 1};
+const int bitfield_sizes_bytes[BUCKET_COUNT] = {124, 63, 32, 16, 8, 4, 2, 1, 1, 1};
+const int bitfield_sizes_bits[BUCKET_COUNT] = {991, 503, 253, 127, 63, 31, 15, 7, 3, 1};
 const unsigned char full_segment = 255;
 __thread void* buckets[BUCKET_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 unsigned char mask = 1;
@@ -44,29 +45,23 @@ xmalloc(size_t bytes)
 	void* page = buckets[bucket_index];
 	void* previous = 0;
 	int bucket_size = bucket_sizes[bucket_index];
-	int bitfield_size = bitfield_sizes[bucket_index];
+	int bitfield_size_bytes = bitfield_sizes_bytes[bucket_index];
+	int bitfield_size_bits = bitfield_sizes_bits[bucket_index];
 	while(page)
 	{
 		void* next = *((void**)page);
 		unsigned char* bitfield = page + sizeof(void*);
-		for(int i = 0; i < bitfield_size; ++i)
+		for(int bit = 0; bit < bitfield_size_bits; ++bit)
 		{
-			if(*bitfield != full_segment)
+			unsigned char shifted_mask = mask << (7 - (bit % 8));
+			if(~bitfield[bit / 8] & shifted_mask)
 			{
-				for(int j = 0; j < 8; ++j)
-				{
-					if(~bitfield[i] & (mask << (7 - j)))
-					{
-						short bit = i*8 + j;
-						bitfield[i] = bitfield[i] | (mask << (7 - j));
-						short* base = (short*)(page + sizeof(void*) + bitfield_sizes[bucket_index] + bit * bucket_size);
-						*base = bucket_index;
-						*(base + 1) = bit;
-						return (base + 2);
-					}
-				}
+				bitfield[bit / 8] = bitfield[bit / 8] | shifted_mask;
+				short* base = (short*)(page + sizeof(void*) + bitfield_size_bytes + bit * bucket_size);
+				*base = bucket_index;
+				*(base + 1) = bit;
+				return (base + 2);
 			}
-			++bitfield;
 		}
 
 		previous = page;
@@ -80,10 +75,10 @@ xmalloc(size_t bytes)
 		*((void**)previous) = new_page;
 
 	*((void**)new_page) = 0;
-	char* bitfield = (char*)(new_page + sizeof(void*));
-	memset(bitfield, 0, bitfield_size);
+	unsigned char* bitfield = (unsigned char*)(new_page + sizeof(void*));
+	memset(bitfield, 0, bitfield_size_bytes);
 	bitfield[0] = 1 << 7;
-	short* base = (short*)((void*)bitfield + bitfield_size);
+	short* base = (short*)((void*)bitfield + bitfield_size_bytes);
 	*base = bucket_index;
 	*(base + 1) = 0;
 	return (base + 2);
@@ -95,7 +90,7 @@ xfree(void* ptr)
 	short index = *((short*)(ptr - 2*sizeof(short)));
 	short offset = *((short*)(ptr - sizeof(short)));
 	int bucket_size = bucket_sizes[index];
-	char* bitfield = ptr - bucket_size * offset - bitfield_sizes[index];
+	unsigned char* bitfield = ptr - bucket_size * offset - bitfield_sizes_bytes[index];
 	bitfield[offset / 8] = bitfield[offset / 8] & ~(mask << (offset % 8));
 }
 
